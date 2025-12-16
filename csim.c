67864c49d42f81b7faef5beeac1142464de76b17
AaronXu <2360679476@qq.com>
2025-12-16 10:24:52 +0800

    feat: :sparkles: 定义缓存set & line ，初始化，根据之间更新

--- csim.c
+++ csim.c
@@ -1,5 +1,44 @@
 #include "cachelab.h"
 
+typedef struct cache_
+{
+    int S,E,B;
+    Cache_line **line;
+} Cache;
+typedef struct cache_line
+{
+    int valid;     //有效位
+    int tag;       //标记位
+    int time_tamp; //时间戳
+} Cache_line;
+void Init_Cache(int s, int E, int b)
+{
+    int S = 1 << s;
+    int B = 1 << b;
+    cache = (Cache *)malloc(sizeof(Cache));
+    cache->S = S;
+    cache->E = E;
+    cache->B = B;
+    cache->line = (Cache_line **)malloc(sizeof(Cache_line *) * S);
+    for (int i = 0; i < S; i++)
+    {
+        cache->line[i] = (Cache_line *)malloc(sizeof(Cache_line) * E);
+        for (int j = 0; j < E; j++)
+        {
+            cache->line[i][j].valid = 0; //初始时，高速缓存是空的
+            cache->line[i][j].tag = -1;
+            cache->line[i][j].time_tamp = 0;
+        }
+    }
+}
+void update(int i, int op_s, int op_tag){
+    cache->line[op_s][i].valid=1;
+    cache->line[op_s][i].tag = op_tag;
+    for(int k = 0; k < cache->E; k++)
+        if(cache->line[op_s][k].valid==1)
+            cache->line[op_s][k].time_tamp++;
+    cache->line[op_s][i].time_tamp = 0;
+}
 int main()
 {
     printSummary(0, 0, 0);
