1752145069dc9dd0573081df7088023d1d3e18a8
AaronXu <2360679476@qq.com>
2025-12-16 15:19:57 +0800

    fix: :bug: 调整到正确的结构体依赖顺序

--- csim.c
+++ csim.c
@@ -1,32 +1,36 @@
 #include "cachelab.h"
 
-typedef struct cache_
-{
-    int S,E,B;
-    Cache_line **line;
-} Cache;
 typedef struct cache_line
 {
     int valid;     //有效位
     int tag;       //标记位
     int time_tamp; //时间戳
 } Cache_line;
+
+typedef struct cache_
+{
+    int S,E,B;
+    Cache_line **line; 
+} Cache; //整个cache 的结构 包含 S 个 set ， 每个 set 有 E 个缓存行， 每行是一个 B 位的块
+
+Cache  *cache; //实例化，一个指向 某个 Cache 的指针
 void Init_Cache(int s, int E, int b)
 {
-    int S = 1 << s;
-    int B = 1 << b;
-    cache = (Cache *)malloc(sizeof(Cache));
+    int S = 1 << s; //set个数： 2 ^ s
+    int B = 1 << b; // 块的大小： 2 ^ b
+    cache = (Cache *)malloc(sizeof(Cache)); //实际cache 实例化， 分配 一个结构体所需要的空间 在堆上 
+    // ( ) 已经free
     cache->S = S;
     cache->E = E;
     cache->B = B;
-    cache->line = (Cache_line **)malloc(sizeof(Cache_line *) * S);
+    cache->line = (Cache_line **)malloc(sizeof(Cache_line *) * S); //二维数组外层初始化： S 个
     for (int i = 0; i < S; i++)
     {
         cache->line[i] = (Cache_line *)malloc(sizeof(Cache_line) * E);
         for (int j = 0; j < E; j++)
         {
             cache->line[i][j].valid = 0; //初始时，高速缓存是空的
-            cache->line[i][j].tag = -1;
+            cache->line[i][j].tag = -1; 
             cache->line[i][j].time_tamp = 0;
         }
     
