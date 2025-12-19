cb5139423b349e34b99c48773277f5d384fc3bfe
AaronXu <2360679476@qq.com>
2025-12-19 09:31:51 +0800

    feat: :sparkles: 补充命令行要求的-h输出help

--- csim.c
+++ csim.c
@@ -154,6 +154,21 @@ void get_trace(int s, int E, int b)
     }
     fclose(pFile);
 }
+void print_help()
+{
+    printf("** A Cache Simulator by Deconx\n");
+    printf("Usage: ./csim-ref [-hv] -s <num> -E <num> -b <num> -t <file>\n");
+    printf("Options:\n");
+    printf("-h         Print this help message.\n");
+    printf("-v         Optional verbose flag.\n");
+    printf("-s <num>   Number of set index bits.\n");
+    printf("-E <num>   Number of lines per set.\n");
+    printf("-b <num>   Number of block offset bits.\n");
+    printf("-t <file>  Trace file.\n\n\n");
+    printf("Examples:\n");
+    printf("linux>  ./csim -s 4 -E 1 -b 4 -t traces/yi.trace\n");
+    printf("linux>  ./csim -v -s 8 -E 2 -b 4 -t traces/yi.trace\n");
+}
 
 int main(int argc, char *argv[])    
 {
